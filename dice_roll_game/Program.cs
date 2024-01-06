05d56a43458e2e1e0b184d0cdfdbb2e960a4a275
Arnie Hsieh <tlhsieh0610@gmail.com>
2024-01-06 12:28:14 +0800

    dice work

--- dice_roll_game/Program.cs
+++ dice_roll_game/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var mainClass = new MainClass();
+mainClass.playGame();
+
+public class MainClass
+{
+
+    private int _tries = 3;
+    private int diceNum = new Random().Next(1, 6);
+    public void playGame()
+    {
+        do
+        {
+            Console.WriteLine("Doce rolled. Guess what number is shows in 3 tries.");
+            //if number is invalid, show “Incorrect input”  “Enter number:”
+            string input = Console.ReadLine();
+            int gussNumber;
+            if (!InputValidator(input, out gussNumber))
+            {
+                Console.WriteLine("Incorrect input");
+                continue;
+            }
+            //check guess
+            bool isSuccessful = GussResult(gussNumber);
+            if (isSuccessful)
+            {
+                break;
+            }
+            else
+            {
+                _tries--;
+                if (_tries == 0)
+                {
+                    Console.WriteLine("You Lose");
+                }
+            }
+
+
+        } while (_tries > 0);
+    }
+
+    private bool InputValidator(string input, out int gussNumber)
+    {
+        bool isValid = int.TryParse(input, out gussNumber) && input != "";
+        return isValid;
+    }
+
+    private bool GussResult(int gussNumber)
+    {
+        if (diceNum == gussNumber)
+        {
+            Console.WriteLine("You Win");
+            return true;
+        }
+        Console.WriteLine("Wrong number");
+        return false;
+    }
+}
